2f9468ade2fd6b71f126b1de0d20dad6156fc32a
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-12-04 23:14:22 +0400

    retrieve previous measurement for activity level calculation

--- TfLbikepoints.py
+++ TfLbikepoints.py
@@ -7,6 +7,7 @@ import database
 import requests
 import datetime
 
+
 logger = logging.getLogger('MyLogger')
 
 
@@ -16,8 +17,33 @@ def measurement(cfg):
                            dbuser=cfg['database']['user'],
                            dbuser_password=cfg['database']['password'],
                            dbname=cfg['database']['name'])
+    prev_data = get_previous_measurement(db)
     data = get_bike_points(cfg)
+    time_stamp = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+    max_sets = len(data)
+    nth_entry = max_sets // 20
+    for idx, entry in enumerate(data):
+        fields, tags = get_fields_tags(entry)
+        save_to_database(db, 'BikePoints', time_stamp, tags, fields)
+        if (idx + 1) % nth_entry == 0 or (idx + 1) == max_sets:
+            logger.info('{} of {} data sets written '
+                        'to database'.format(idx + 1, max_sets))
     save_to_database(db, data)
+
+
+def get_previous_measurement(db):
+    query = 'SELECT "NbDocks", "NbBikes", "NbEmptyDocks" FROM "BikePoints" ' \
+            'GROUP BY id ORDER BY DESC LIMIT 1;'
+    result = db.client.query(query)
+    prev_data = []
+    for bike_point in result.raw['series']:
+        bike_point_id = bike_point['tags']['id']
+        cols = bike_point['columns']
+        for values in bike_point['values']:
+            vals = values
+        fields = dict(zip(cols, vals))
+        prev_data.append({bike_point_id: fields})
+    return prev_data
 
 
 def get_bike_points(cfg):
@@ -31,48 +57,42 @@ def get_bike_points(cfg):
     return data
 
 
-def save_to_database(db, data):
-    logger.info('saving data sets to database')
-    time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    max_sets = len(data)
-    nth_entry = max_sets // 20
-    for idx, entry in enumerate(data):
-        # flatten data structure
-        fields = dict(
-            id=entry['id'],
-            commonName=entry['commonName'],
-            lon=entry['lon'], lat=entry['lat'],
-        )
-        for prop in entry['additionalProperties']:
-            fields[prop['key']] = prop['value']
-            fields[prop['key'] + '_modified'] = prop['modified']
-        # convert type for some attributes
-        fields['NbBikes'] = int(fields['NbBikes'])
-        fields['NbDocks'] = int(fields['NbDocks'])
-        fields['NbEmptyDocks'] = int(fields['NbEmptyDocks'])
-        fields['Locked'] = fields['Locked'] in ['True', 'true']
-        fields['Installed'] = fields['Installed'] in ['True', 'true']
-        fields['Temporary'] = fields['Temporary'] in ['True', 'true']
-        # add calculated data
-        fields['BrokenDocks'] = fields['NbDocks'] - \
-                                fields['NbBikes'] - fields['NbEmptyDocks']
-        fields['percentageBikes'] = fields['NbBikes'] / fields['NbDocks']
-        fields['percentageBrokenDocks'] = fields['BrokenDocks'] / \
-                                          fields['NbDocks']
+def get_fields_tags(entry):
+    fields = dict(
+        id=entry['id'],
+        commonName=entry['commonName'],
+        lon=entry['lon'], lat=entry['lat'],
+    )
+    for prop in entry['additionalProperties']:
+        fields[prop['key']] = prop['value']
+        fields[prop['key'] + '_modified'] = prop['modified']
+    # convert type for some attributes
+    fields['NbBikes'] = int(fields['NbBikes'])
+    fields['NbDocks'] = int(fields['NbDocks'])
+    fields['NbEmptyDocks'] = int(fields['NbEmptyDocks'])
+    fields['Locked'] = fields['Locked'] in ['True', 'true']
+    fields['Installed'] = fields['Installed'] in ['True', 'true']
+    fields['Temporary'] = fields['Temporary'] in ['True', 'true']
+    # add calculated data
+    fields['BrokenDocks'] = fields['NbDocks'] - \
+                            fields['NbBikes'] - fields['NbEmptyDocks']
+    fields['percentageBikes'] = fields['NbBikes'] / fields['NbDocks']
+    fields['percentageBrokenDocks'] = fields['BrokenDocks'] / \
+                                      fields['NbDocks']
+    # some fields are tags
+    tags = {}
+    for key in ['TerminalName', 'commonName', 'id']:
+        tags[key] = fields[key]
+        fields.pop(key, None)  # remove fields that are now tags
+    return fields, tags
 
-        # some fields are tags
-        tags = {}
-        for key in ['TerminalName', 'commonName', 'id']:
-            tags[key] = fields[key]
-            fields.pop(key, None)
-        # write to database
-        data_json = [{
-            'measurement': 'BikePoints',
-            "tags": tags,
-            'time': time_stamp,
-            'fields': fields
-        }]
-        db.write(data_json)
-        if (idx + 1) % nth_entry == 0 or (idx + 1) == max_sets:
-            logger.info('{} of {} data sets written '
-                        'to database'.format(idx + 1, max_sets))
+
+def save_to_database(db, measurement, time_stamp, tags, fields):
+    # write to database
+    data_json = [{
+        'measurement': measurement,
+        'time': time_stamp,
+        "tags": tags,
+        'fields': fields
+    }]
+    db.write(data_json)
